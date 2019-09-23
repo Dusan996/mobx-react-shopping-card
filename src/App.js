beee3908c8bcd48b6032ec942a99f4f1e5dfc666
Dusan996 <dvulic996@gmail.com>
2019-09-23 11:46:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
+import LandingPage from './components/LandingPage';
+import ProductOverview from './components/ProductOverview';
+import ShoppingCard from './components/ShoppingCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+@inject('Product')
+@observer
+
+class App extends Component {
+  render() {
+    const { selectedProduct } = this.props.Product;
+    const { shoppingCard } = this.props.Product;
+    return (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route exact path='/' component={LandingPage} />
+            <Route path={`/product`} component={() => <ProductOverview product={selectedProduct} />} />
+            <Route path={`/shopcard`} component={() => <ShoppingCard shoppingCard={shoppingCard} />} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
